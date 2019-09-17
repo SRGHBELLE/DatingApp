cccfbd7a48b764b80476fbeab95c11a7a5fd9c62
sandra <srbella17@gmail.com>
2019-09-17 15:55:47 -0400

    section 5

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -45,6 +45,8 @@ namespace DatingApp.API.Controllers
         [HttpPost("Login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
+            throw new Exception("Computer says no!");
+
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if (userFromRepo == null)
--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -9,12 +9,16 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Diagnostics;
+using System.Net;
+using DatingApp.API.Helpers;
 
 namespace DatingApp.API
 {
@@ -56,8 +60,18 @@ namespace DatingApp.API
             }
             else
             {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                //app.UseHsts();
+               app.UseExceptionHandler(builder => {
+                   builder.Run(async context => {
+                       context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                       var error = context.Features.Get<IExceptionHandlerFeature>();
+                       if (error != null)
+                       {
+                           context.Response.AddApplicationError(error.Error.Message);
+                           await context.Response.WriteAsync(error.Error.Message);
+                       }
+                   });
+               });
             }
 
             //app.UseHttpsRedirection();
